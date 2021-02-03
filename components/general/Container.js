7156833d4e0861049114616a0edf4f6d421eab40
Blake Lam <blake@blakelam.com>
2021-02-03 12:40:36 -0600

    Adjust container width

--- components/general/Container.js
+++ components/general/Container.js
@@ -4,7 +4,7 @@ import { Box } from '@/components/core'
 export const Container = ({ className, ...props }) => (
     <Box
         className={cx(
-            'w-full max-w-screen-2xl mx-auto my-0 px-6 sm:px-8',
+            'w-full max-w-screen-xl mx-auto my-0 px-6 sm:px-8',
             className
         )}
         {...props}
