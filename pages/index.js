40ff7181ccc75182bf862b5878c86cf9d56b6677
Blake Lam <blake@blakelam.com>
2021-02-11 08:26:43 -0600

    Add container to starting page

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,44 @@
 import { withStore } from '@/lib/store'
-import { Link } from '@/components/core'
-import { Layout } from '@/components/general'
+import { Link, Flex } from '@/components/core'
+import { Layout, Container } from '@/components/general'
 
 const Home = ({ store }) => (
     <Layout>
-        <Link href="/pih">PIH - Landing Page</Link>
-        <Link href="/pih/patient">PIH - Patient - Instructions</Link>
-        <Link href="/pih/patient/request">PIH - Patient - Request Form</Link>
-        <Link href="/pih/thirdparty">PIH - Third Party - Instructions</Link>
-        <Link href="/pih/thirdparty/request">
-            PIH - Third Party - Request Form
-        </Link>
+        <Flex as={Container} className="flex-col">
+            <Link href="/pih">PIH - Landing Page</Link>
+            <Link href="/pih/patient">PIH - Patient - Instructions</Link>
+            <Link href="/pih/patient/request">
+                PIH - Patient - Request Form
+            </Link>
+            <Link href="/pih/thirdparty">PIH - Third Party - Instructions</Link>
+            <Link href="/pih/thirdparty/request">
+                PIH - Third Party - Request Form
+            </Link>
 
-        <div>Count: {store.state.count}</div>
+            <div>Count: {store.state.count}</div>
 
-        <button
-            onClick={() =>
-                store.dispatch({
-                    type: 'UPDATE_COUNT',
-                    value: store.state.count - 1,
-                })
-            }
-        >
-            &ndash;
-        </button>
+            <button
+                onClick={() =>
+                    store.dispatch({
+                        type: 'UPDATE_COUNT',
+                        value: store.state.count - 1,
+                    })
+                }
+            >
+                &ndash;
+            </button>
 
-        <button
-            onClick={() =>
-                store.dispatch({
-                    type: 'UPDATE_COUNT',
-                    value: store.state.count + 1,
-                })
-            }
-        >
-            +
-        </button>
+            <button
+                onClick={() =>
+                    store.dispatch({
+                        type: 'UPDATE_COUNT',
+                        value: store.state.count + 1,
+                    })
+                }
+            >
+                +
+            </button>
+        </Flex>
     </Layout>
 )
 
