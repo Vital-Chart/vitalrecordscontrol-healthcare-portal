d2a5fd3e9b7c8904424776bd03db4882e1624dbe
Blake Lam <blake@blakelam.com>
2021-02-16 12:34:24 -0600

    Clean up footer

--- components/general/Footer.js
+++ components/general/Footer.js
@@ -2,24 +2,32 @@ import { Box, Flex, Text, Link, Image } from '@/components/core'
 import { Container } from '@/components/general'
 
 export const Footer = () => (
-    <Box as="footer" className="py-8 bg-black">
-        <Container>
-            <Box className="w-40 mx-auto sm:mx-0 mb-6">
+    <Box as="footer" className="py-6 bg-black">
+        <Flex
+            as={Container}
+            className="flex-col md:flex-row md:justify-between items-center"
+        >
+            <Box className="w-40 mb-6 md:mb-0">
                 <Link href="https://vitalrecordscontrol.com/">
                     <Image src="/images/vrc-logo.png" />
                 </Link>
             </Box>
-            <Text className="text-sm text-center sm:text-left text-white">
-                &copy;{new Date().getFullYear()} Vital Records Control. All
-                Rights Reserved. .
-            </Text>
-            <Link
-                href="https://vitalrecordscontrol.com/privacy-policy/"
-                className="block py-2 underline text-sm text-center sm:text-left text-white"
-            >
-                Website Privacy Policy
-            </Link>
-        </Container>
+            <Box>
+                <Text
+                    as="p"
+                    className="pb-2 text-sm text-center md:text-right text-white"
+                >
+                    &copy;{new Date().getFullYear()} Vital Records Control. All
+                    Rights Reserved.
+                </Text>
+                <Link
+                    href="https://vitalrecordscontrol.com/privacy-policy/"
+                    className="block underline text-sm text-center md:text-right text-white"
+                >
+                    Website Privacy Policy
+                </Link>
+            </Box>
+        </Flex>
     </Box>
 )
 
