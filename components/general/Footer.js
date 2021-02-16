d4b4c11d8c590db2b786d8c64aa3b6da6512c137
Blake Lam <blake@blakelam.com>
2021-02-16 10:12:50 -0600

    Update footer - copyright info and logo

--- components/general/Footer.js
+++ components/general/Footer.js
@@ -1,12 +1,24 @@
-import { Box, Text } from '@/components/core'
+import { Box, Flex, Text, Link, Image } from '@/components/core'
 import { Container } from '@/components/general'
 
 export const Footer = () => (
-    <Box as="footer" className="py-4 bg-black">
+    <Box as="footer" className="py-8 bg-black">
         <Container>
-            <Text className="text-center text-white">
-                &copy;{new Date().getFullYear()} VRC Healthcare Portal
+            <Box className="w-40 mx-auto sm:mx-0 mb-6">
+                <Link href="https://vitalrecordscontrol.com/">
+                    <Image src="/images/vrc-logo.png" />
+                </Link>
+            </Box>
+            <Text className="text-sm text-center sm:text-left text-white">
+                &copy;{new Date().getFullYear()} Vital Records Control. All
+                Rights Reserved. .
             </Text>
+            <Link
+                href="#"
+                className="block py-2 underline text-sm text-center sm:text-left text-white"
+            >
+                Website Privacy Policy
+            </Link>
         </Container>
     </Box>
 )
