abc71681aee4ae4e69cd621f6c1fcd8dc5da7c22
Blake Lam <blake@blakelam.com>
2021-02-19 13:37:10 -0600

    Update links on dev landing page

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,32 @@ import { Layout, Container } from '@/components/general'
 
 const Home = ({ store }) => (
     <Layout>
-        <Flex as={Container} className="flex-col">
+        <Flex as={Container} className="flex-col space-y-8">
             <Link href="/pih">PIH - Landing Page</Link>
-            <Link href="/pih/patient">PIH - Patient - Instructions</Link>
-            <Link href="/pih/patient/request">
-                PIH - Patient - Request Form
-            </Link>
-            <Link href="/pih/sendto">PIH - Send To - Instructions</Link>
-            <Link href="/pih/sendto/form">PIH - Send To - Request Form</Link>
+            <Flex className="flex-col">
+                <Link href="/pih/patient">PIH - Patient - Instructions</Link>
+                <Link href="/pih/patient/form">
+                    PIH - Patient - Request Form
+                </Link>
+                <Link href="/pih/patient/upload">
+                    PIH - Patient - Upload Authorization
+                </Link>
+                <Link href="/pih/patient/review">
+                    PIH - Patient - Review & Submit
+                </Link>
+            </Flex>
+            <Flex className="flex-col">
+                <Link href="/pih/sendto">PIH - Send To - Instructions</Link>
+                <Link href="/pih/sendto/form">
+                    PIH - Send To - Request Form
+                </Link>
+                <Link href="/pih/sendto/upload">
+                    PIH - Patient - Upload Authorization
+                </Link>
+                <Link href="/pih/sendto/review">
+                    PIH - Patient - Review & Submit
+                </Link>
+            </Flex>
         </Flex>
     </Layout>
 )
