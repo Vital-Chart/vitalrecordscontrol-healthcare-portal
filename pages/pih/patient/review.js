d8ceaa841f7bc4ee962e5eb9d8b1d402e4b3fffc
Blake Lam <blake@blakelam.com>
2021-02-19 10:09:51 -0600

    Setup redirects for patient/upload and patient/review

--- pages/pih/patient/review.js
+++ pages/pih/patient/review.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { withStore } from '@/lib/store'
 import { LayoutReview } from '@/components/layouts'
 
 const PIHPatientReview = ({ store }) => {
+    const router = useRouter()
+    useEffect(() => {
+        // Get hospital name from first directory after 'pages' root
+        const hospital = router.pathname.split('/')[1]
+        // Redirect to hospital landing page if no tracking number exists
+        if (!store.state.form.TRKNUM && !store.state.form.uploadedFiles) {
+            router.push(`/${hospital}`)
+        }
+    })
+
     return <LayoutReview />
 }
 
--- pages/pih/patient/upload.js
+++ pages/pih/patient/upload.js
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { withStore } from '@/lib/store'
 import { LayoutUpload } from '@/components/layouts'
 import { Box, Text } from '@/components/core'
 
 const PIHPatientUpload = ({ store }) => {
+    const router = useRouter()
+    useEffect(() => {
+        // Get hospital name from first directory after 'pages' root
+        const hospital = router.pathname.split('/')[1]
+        // Redirect to hospital landing page if no tracking number exists
+        if (!store.state.form.TRKNUM) {
+            router.push(`/${hospital}`)
+        }
+    })
+
     return (
         <LayoutUpload>
             <Box className="pb-8 border-b border-gray-light">
