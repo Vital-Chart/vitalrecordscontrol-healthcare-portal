ad9c874ec05a354afcc17a27f735111a00bd84ec
Kyle Brumm <kjbrumm@gmail.com>
2021-02-26 09:37:59 -0600

    Tweak server errors

--- components/atoms/ServerErrorList.js
+++ components/atoms/ServerErrorList.js
@@ -3,15 +3,16 @@ import { Box, Flex, Text } from '@/components/core'
 import IconAlert from '@/icons/icon-alert.svg'
 
 const errorMessages = {
-    100000: '100000: General server error',
+    100000: '100000: Something went wrong',
     100001: '100001: Invalid Tracking number provided',
-    100002: '100002: A class of error reflecting a data validation failure.  For example, a combo box value was not found in spec',
+    // 100002: '100002: A class of error reflecting a data validation failure. For example, a combo box value was not found in spec',
+    100002: '100002: Information provided is invalid',
     100003: '100003: Problem encountered downloading one of more the supplied files',
     100004: '100004: Invalid signature',
     100005: '100005: Invalid file name given',
-    100006: '100006: No Tracking Number Information and No Facility Information supplied',
+    100006: '100006: No tracking number information and no facility information supplied',
     100007: '100007: Invalid file name given',
-    100007: '100008: Tracking Number ID not found',
+    100008: '100008: Tracking number ID not found',
 }
 
 export const ServerErrorList = ({ className, errors }) => {
