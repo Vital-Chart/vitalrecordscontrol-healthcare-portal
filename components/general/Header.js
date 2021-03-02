0dd0676c522ec3eaeb844437d318881e0315b3e5
Blake Lam <blake@blakelam.com>
2021-03-02 13:30:08 -0600

    Adjust header to be more suitable to  a variety of logos

--- components/general/Header.js
+++ components/general/Header.js
@@ -1,37 +1,58 @@
-import { useStore } from '@/lib/store'
+import cx from 'classnames'
 import useNavigation from '@/lib/useNavigation'
 import hospitals from '@/lib/hospitals'
 import { Box, Flex, Link, Image } from '@/components/core'
 import { Container } from '@/components/general'
 
 export const Header = () => {
-    const { hospital, getLandingPage, getContactPage } = useNavigation()
+    const {
+        hospital,
+        getLandingPage,
+        getContactPage,
+        isLandingPage,
+    } = useNavigation()
+
+    console.log({ isLandingPage })
 
     return (
-        <Box as="header" className="bg-black mb-20">
-            <Flex as={Container} className="relative">
-                {hospitals[hospital] && (
-                    <Link
-                        href={getLandingPage()}
-                        className="block absolute top-0 left-6 sm:left-8 p-6 bg-white shadow"
-                    >
-                        <Image
-                            className="h-16 w-auto"
-                            src={hospitals[hospital].logo}
-                        />
-                    </Link>
-                )}
-
-                <Flex className="flex-col ml-auto py-4 text-xs text-white text-right">
-                    <Link href="/">VitalChart® Virtual ROI Portal</Link>
-
-                    {hospital && (
-                        <Link href={getContactPage()} className="underline">
-                            Contact Us
+        <Box as="header">
+            <Box className="bg-black">
+                <Container className="py-2 text-sm text-white text-right">
+                    <Box className="ml-auto">
+                        {hospital && (
+                            <Link
+                                href={getContactPage()}
+                                className="underline mr-4"
+                            >
+                                Contact Us
+                            </Link>
+                        )}
+                        <Link href="/" className="text-gray">
+                            VitalChart® Virtual ROI Portal
                         </Link>
-                    )}
-                </Flex>
-            </Flex>
+                    </Box>
+                </Container>
+            </Box>
+            {hospitals[hospital] && (
+                <Container>
+                    <Flex
+                        className={cx(
+                            'py-6',
+                            isLandingPage && 'justify-center'
+                        )}
+                    >
+                        <Link href={getLandingPage()}>
+                            <Image
+                                className={cx(
+                                    'w-auto',
+                                    isLandingPage ? 'h-24' : 'h-20'
+                                )}
+                                src={hospitals[hospital].logo}
+                            />
+                        </Link>
+                    </Flex>
+                </Container>
+            )}
         </Box>
     )
 }
