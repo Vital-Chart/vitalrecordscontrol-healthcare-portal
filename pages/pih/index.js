37e74f0180fe0ff1245bbdbca42c9b4c49840842
Blake Lam <blake@blakelam.com>
2021-03-15 13:51:34 -0500

    Add top section to landing page

--- pages/pih/index.js
+++ pages/pih/index.js
@@ -4,12 +4,49 @@ import { PageHeading, Info } from '@/components/atoms'
 import { Layout, Container } from '@/components/general'
 
 const Hospital = () => {
-    const { getLandingPage } = useNavigation()
+    const { hospital, getLandingPage } = useNavigation()
 
     return (
         <Layout>
-            <Container>
-                <PageHeading className="text-center pt-4 mb-6">
+            <Flex className="py-32 items-center bg-landing-bg bg-center bg-cover">
+                <Container>
+                    <Box className="max-w-xl text-center sm:text-left">
+                        <Heading
+                            as="h1"
+                            variant="h1"
+                            className="pt-4 pb-6 mb-6 text-white border-b-2 border-white"
+                        >
+                            Request Medical Records
+                        </Heading>
+                        <Text className="text-lg sm:text-xl text-white">
+                            Through our online patient portal, VitalChart
+                            Virtual ROI, patients can request medical records
+                            for themselves or send them to a third-party.
+                        </Text>
+                        <Box className="mt-6 -ml-2">
+                            <Button
+                                as={Link}
+                                href={`${hospital}/#newRequest`}
+                                variant="filledSecondary"
+                                className="flex-grow m-2 text-center"
+                            >
+                                Start New Request
+                            </Button>
+
+                            <Button
+                                as={Link}
+                                href={getLandingPage()}
+                                variant="filledSecondary"
+                                className="flex-grow m-2 text-center"
+                            >
+                                Track Request or Download Records
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </Flex>
+            <Container id="newRequest">
+                <PageHeading className="text-center pt-12 mb-6">
                     Order Your Medical Records Online
                 </PageHeading>
 
