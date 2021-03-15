9bd48f7792636f4c0f420dda91dabd44799ea328
Blake Lam <blake@blakelam.com>
2021-03-15 13:31:01 -0500

    Clean up hmac stuff

--- pages/api/generateHmac.js
+++ pages/api/generateHmac.js
@@ -19,29 +19,21 @@ export default async function handler(req, res) {
 
     let payload
 
-    const trackingNumber = fields['TRKNUM']
-
-    // Get only the first facility ID
-    const facilityId = fields['FI_CB'].slice(0, 7)
-
-    if (fields['hmacType'] === 'facilityId') {
-        payload = facilityId
-    }
-
-    if (fields['hmacType'] === 'trackingFileName') {
-        payload = [trackingNumber, fields['fileName']].join(':')
-    }
-
-    if (fields['hmacType'] === 'tracking') {
-        payload = trackingNumber
+    switch (fields['hmacType']) {
+        case 'createRequest':
+            payload = fields['FI_CB'].split(',', 1).toString()
+            break
+        case 'deleteUploadedFile':
+            payload = [fields['TRKNUM'], fields['fileName']].join(':')
+            break
+        default:
+            payload = fields['TRKNUM']
     }
 
     const hmac = crypto
         .createHmac('sha256', 'portal-test')
         .update(payload)
         .digest('hex')
-
-    // console.log({ payload, hmac })
 
     return res.status(200).json({ hmac })
 }
