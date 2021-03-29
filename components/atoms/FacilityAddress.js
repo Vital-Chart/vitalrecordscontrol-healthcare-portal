eb6b4ad7eda8ee703058670427b8464150566b5c
Blake Lam <blake@blakelam.com>
2021-03-29 11:38:43 -0500

    Update hospital contact

--- components/atoms/FacilityAddress.js
+++ components/atoms/FacilityAddress.js
@@ -1,7 +1,7 @@
 import { Box, Text, Link } from '@/components/core'
 
 export const FacilityAddress = ({ facility, className }) => {
-    const { id, name, phone, address1, address2, address3, address4 } = facility
+    const { id, name, phone, address, email } = facility
     const phoneNum = phone.split('x')[0].replace(/\D/g, '')
     const phoneExt = phone.split('x')[1]
     let phoneLink = `tel:${phoneNum}`
@@ -13,10 +13,9 @@ export const FacilityAddress = ({ facility, className }) => {
     return (
         <Box key={id} className={className}>
             <Text className="font-bold">{name}</Text>
-            <Text>{address1}</Text>
-            {address2 && <Text>{address2}</Text>}
-            {address3 && <Text>{address3}</Text>}
-            {address4 && <Text>{address4}</Text>}
+            {address.map(addressLine => (
+                <Text>{addressLine}</Text>
+            ))}
             <Link href={phoneLink}>{phone}</Link>
         </Box>
     )
