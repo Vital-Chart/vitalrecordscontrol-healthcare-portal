c7ad016be47a5b820e84eb5bb62d8850c59683e9
Blake Lam <blake@blakelam.com>
2021-03-29 14:43:05 -0500

    Configure endpoints by environment

--- pages/envtest.js
+++ pages/envtest.js
@@ -1,8 +1,7 @@
 const Test = () => (
     <>
         <h1>Env Test</h1>
-        <p>process.env.NEXT_PUBLIC_Test:</p>
-        <p>{process.env.NEXT_PUBLIC_Test}</p>
+        <p>process.env.NEXT_PUBLIC_ENV: {process.env.NEXT_PUBLIC_ENV}</p>
     </>
 )
 
