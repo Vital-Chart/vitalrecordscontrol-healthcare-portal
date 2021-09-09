624ae05c0b58116609acfcb9b9cfe3787a021f88
Blake Lam <blake@blakelam.com>
2021-09-09 11:50:20 -0500

    Disabled pickup for UCSF

--- pages/ucsf/patient/form.js
+++ pages/ucsf/patient/form.js
@@ -50,12 +50,8 @@ import IconClose from '@/icons/icon-close.svg'
 import IconLoading from '@/icons/icon-loading.svg'
 
 const Form = ({ store }) => {
-    const {
-        getLandingPage,
-        goToStep,
-        getContactPage,
-        hospital,
-    } = useNavigation()
+    const { getLandingPage, goToStep, getContactPage, hospital } =
+        useNavigation()
 
     const methods = useForm({ defaultValues: store.state.form })
     const {
@@ -117,11 +113,8 @@ const Form = ({ store }) => {
         setIsFetching(true)
 
         try {
-            const {
-                trackingNumbers,
-                errorInformation,
-                inError,
-            } = await createRequest(store.state.form)
+            const { trackingNumbers, errorInformation, inError } =
+                await createRequest(store.state.form)
 
             if (inError) {
                 setServerErrors(
@@ -783,7 +776,7 @@ const Form = ({ store }) => {
                                             <option value="PS">
                                                 CD via US Postal Service
                                             </option>
-                                            <option value="PU">
+                                            <option value="PU" disabled>
                                                 CD for On-Site Pickup
                                             </option>
                                         </Select>
